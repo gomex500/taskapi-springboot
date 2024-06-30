9d190498fcc5c58ca282925e5892daf267304a9a
gomex500 <gomezfreddy886@gmail.com>
2024-06-29 23:54:46 -0600

    otro save

--- src/main/java/com/example/taskapi/controllers/TaskController.java
+++ src/main/java/com/example/taskapi/controllers/TaskController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
     public List<Task> getAllTask(){
         return taskService.getAllTasks();
